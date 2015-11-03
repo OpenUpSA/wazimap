6672fd9c9cc2f2b9fe3716315670217e6121e856
Greg Kempe <greg@kempe.net>
2015-11-03 13:12:51 +0200

    Use DB URL from env

--- censusreporter/api/config.py
+++ censusreporter/api/config.py
@@ -1,7 +1,9 @@
 # Database
+import os
 
 DB_NAME = 'census_2011'
 DB_USER = 'census'
 DB_PASSWORD = 'census'
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://census:census@localhost/census_2011')
 
 WARD_SEARCH_ENDPOINT = 'http://wards.code4sa.org/'
