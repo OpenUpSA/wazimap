ae77d4cbc6a64ad5ddb272a6c6e7819076d13cb5
Greg Kempe <greg@kempe.net>
2016-02-07 15:43:41 +0200

    python package files

---
+++ setup.py
@@ -0,0 +1,101 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+setup(
+    name='wazimap',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=version,
+
+    description='A Django application for exploring census and other place-specific data',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/Code4SA/wazimap',
+
+    # Author details
+    author='Code for South Africa',
+    author_email='greg@code4sa.org',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['docs', 'tests*']),
+
+    #include_package_data=True,
+    #package_data={
+    #    'cobalt': ['*.xsl'],
+    #},
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'Django==1.5.4',
+        'Fabric==1.8.3',
+        'GDAL==1.11.0',
+        'SQLAlchemy==0.9.4',
+        'boto==2.27.0',
+        'django-pipeline==1.3.23',
+        'ecdsa==0.11',
+        'futures==2.1.6',
+        'gevent==1.0.1',
+        'greenlet==0.4.6',
+        'gunicorn==18.0',
+        'newrelic==2.18.1.15',
+        'numpy==1.7.0',
+        'paramiko==1.12.3',
+        'psycopg2==2.5.2',
+        'pycrypto==2.6.1',
+        'python-memcached==1.53',
+        'requests==1.2.0',
+        'unicodecsv==0.9.4',
+        'whitenoise==1.0.6',
+        'wsgiref==0.1.2',
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['nose', 'flake8'],
+        'test': ['nose', 'flake8'],
+    },
+)
