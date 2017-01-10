4f3a002c130999e0215e053030c9c5ea80f3aacd
Greg Kempe <greg@kempe.net>
2017-01-10 14:27:57 +0200

    Further tests for get_stat_data

--- wazimap/tests/data/test_utils.py
+++ wazimap/tests/data/test_utils.py
@@ -1,5 +1,6 @@
 from wazimap.tests.support import WazimapTestCase
 from wazimap.data.utils import get_stat_data
+from wazimap.data.tables import FieldTable
 
 
 class UtilsTestCase(WazimapTestCase):
@@ -40,3 +41,17 @@ lev,code,Female,
         self.assertEqual(data['Male']['values']['this'], 10)
         self.assertIsNone(data['Female']['numerators']['this'])
         self.assertIsNone(data['Female']['values']['this'])
+
+    def test_get_stat_data_nulls_with_denominator_key(self):
+        table = FieldTable(['household goods'], universe='Households', denominator_key='total households')
+        self.load_data(table, """
+lev,code,fridge,10
+lev,code,computer,5
+lev,code,total households,
+""")
+        data, total = get_stat_data(['household goods'], 'lev', 'code', self.s)
+        self.assertEqual(total, None)
+        self.assertEqual(data['Fridge']['numerators']['this'], 10)
+        self.assertIsNone(data['Fridge']['values']['this'])
+        self.assertEqual(data['Computer']['numerators']['this'], 5)
+        self.assertIsNone(data['Computer']['values']['this'])
