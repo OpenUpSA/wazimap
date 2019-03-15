c861933b4ce321439f782d7b62d877f11f916481
Phillip Ahereza <pahereza@gmail.com>
2019-03-15 10:46:51 +0300

    added migrations to port to python 3, remove  from non-bytestrings

--- wazimap/data/utils.py
+++ wazimap/data/utils.py
@@ -9,6 +9,15 @@ from django.conf import settings
 from django.db.backends.base.creation import TEST_DATABASE_PREFIX
 from django.db import connection
 
+# See http://docs.sqlalchemy.org/en/latest/core/constraints.html#constraint-naming-conventions
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 if settings.TESTING:
     # Hack to ensure the sqlalchemy database name matches the Django one
     # during testing
@@ -23,20 +32,12 @@ if settings.TESTING:
     parts[-1] = db_name
     url = '/'.join(parts)
     _engine = create_engine(url)
+    _metadata = MetaData(bind=_engine, naming_convention=naming_convention)
 else:
     _engine = create_engine(settings.DATABASE_URL)
+    _metadata = MetaData(bind=_engine, naming_convention=naming_convention, reflect=True)
 
 
-# See http://docs.sqlalchemy.org/en/latest/core/constraints.html#constraint-naming-conventions
-naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
-
-_metadata = MetaData(bind=_engine, naming_convention=naming_convention)
 _Session = sessionmaker(bind=_engine)
 
 
