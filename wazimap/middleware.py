c53dda8dbe115a3b538112e10b17baa356601e25
paulmwatson <github.com@paulmwatson.com>
2023-08-02 23:15:17 +0200

    Fixed depreciated middleware

--- wazimap/middleware.py
+++ wazimap/middleware.py
@@ -3,10 +3,24 @@ from django.http.response import HttpResponsePermanentRedirect
 
 
 class RedirectMiddleware(object):
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
         host = request.get_host()
 
         # redirect www.foo.com to foo.com?
         if settings.STRIP_WWW and host.startswith("www."):
             redirect_url = '%s://%s%s' % (request.scheme, host[4:], request.get_full_path())
             return HttpResponsePermanentRedirect(redirect_url)
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
